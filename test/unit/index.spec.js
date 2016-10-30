b9d61d312071811b41704896ca6291f433b0e474
Malik <malik.1does@gmail.com>
2016-10-30 19:51:07 +0100

    feat(tests): added some unit and e2e tests

---
+++ test/unit/index.spec.js
@@ -0,0 +1,8 @@
+var taaspe = require('../../libs/taaspe.js');
+var expect = require('chai').expect;
+
+describe("Taaspe", function () {
+  it("Should be defined", function (){
+    expect(taaspe).to.be.ok;
+  });
+})
